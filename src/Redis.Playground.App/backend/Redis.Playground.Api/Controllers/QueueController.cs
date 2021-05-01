b0f73b61d6cb9ea7b50005e483cf30557434d941
James Tayler <james@tayler.io>
2021-05-02 08:56:34 +1200

    fix namespace issue

--- src/Redis.Playground.App/backend/Redis.Playground.Api/Controllers/QueueController.cs
+++ src/Redis.Playground.App/backend/Redis.Playground.Api/Controllers/QueueController.cs
@@ -1,22 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Redis.Playground.Libs.Queue;
 
-namespace Redis.Playround.Api.Controllers
+namespace Redis.Playground.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class QueueController : ControllerBase
     {
         
-        private readonly ILogger<VoteController> _logger;
+        private readonly ILogger<QueueController> _logger;
         private readonly IQueue _queue;
 
-        public QueueController(ILogger<VoteController> logger, IQueue queue)
+        public QueueController(ILogger<QueueController> logger, IQueue queue)
         {
             _logger = logger;
             _queue = queue;
@@ -41,7 +39,6 @@ namespace Redis.Playround.Api.Controllers
             
             if(count <= 0 || count >= int.MaxValue)
                 throw new ArgumentOutOfRangeException(nameof(count));
-
             
             var size = default(long);
             
